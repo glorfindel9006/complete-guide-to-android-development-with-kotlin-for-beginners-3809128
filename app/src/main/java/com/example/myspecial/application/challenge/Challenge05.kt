616bbc5d04208d1045d02fda25c7ad9b1323b4ad
glorfindel9006 <galadion@tutanota.com>
2025-01-28 17:26:56 +0700

    Update with solution

--- app/src/main/java/com/example/myspecial/application/challenge/Challenge05.kt
+++ app/src/main/java/com/example/myspecial/application/challenge/Challenge05.kt
@@ -1,17 +1,21 @@
 package com.example.myspecial.application.challenge
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ShoppingCart
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -24,7 +28,9 @@ import com.example.myspecial.application.ui.theme.MySpecialApplicationTheme
 @Composable
 fun MyComposable() {
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
         // choose the correct alignments
     ) {
         Image(
@@ -35,25 +41,27 @@ fun MyComposable() {
             contentScale = ContentScale.Crop,
             contentDescription = "App logo"
         )
-        Column(
+        Row(
             modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
             // choose the correct alignments
         ) {
             Icon(
-                imageVector = , // choose the correct icon
+                Icons.Rounded.ShoppingCart, // choose the correct icon
                 contentDescription = "Shopping cart icon",
             )
             Text(
-                text = "<Update me!>",
+                text = "Free shipping on all orders",
                 modifier = Modifier.padding(16.dp),
             )
         }
         Button(
-            modifier = , // should expand horizontally and have padding
+            modifier = Modifier.fillMaxWidth().padding(16.dp), // should expand horizontally and have padding
             onClick = { /* Ignore */ }
         ) {
             Text(
-                text = "<Update me!>",
+                text = "Let's go!",
                 fontWeight = FontWeight.Bold,
                 style = MaterialTheme.typography.headlineMedium
             )
