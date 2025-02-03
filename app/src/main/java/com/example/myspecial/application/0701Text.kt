023c9137b5605b2e3a5bf7c20388c73dad176d2c
glorfindel9006 <galadion@tutanota.com>
2025-02-03 14:18:35 +0700

    Text properties test

--- app/src/main/java/com/example/myspecial/application/0701Text.kt
+++ app/src/main/java/com/example/myspecial/application/0701Text.kt
@@ -9,9 +9,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myspecial.application.ui.theme.AppTheme
 
 @Composable
@@ -24,8 +27,10 @@ fun TextComposable(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Text(
-            text = "Hello, Learners!",
-            style = MaterialTheme.typography.headlineLarge
+            text = stringResource(R.string.hello_learners),
+            style = MaterialTheme.typography.displayLarge,
+            letterSpacing = 2.sp,
+            textDecoration = TextDecoration.Underline
         )
     }
 }
