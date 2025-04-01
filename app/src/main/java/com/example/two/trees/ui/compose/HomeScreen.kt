1c064d1d4857d4a396c796841377002981da6ad4
glorfindel9006 <galadion@tutanota.com>
2025-04-01 16:51:09 +0800

    Implement solution.

--- app/src/main/java/com/example/two/trees/ui/compose/HomeScreen.kt
+++ app/src/main/java/com/example/two/trees/ui/compose/HomeScreen.kt
@@ -3,6 +3,7 @@ package com.example.two.trees.ui.compose
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
@@ -28,6 +29,7 @@ import com.example.two.trees.ui.theme.AppTheme
 
 @Composable
 fun HomeScreen(
+    takeTourClickAction: () -> Unit,
     modifier: Modifier = Modifier,
     scrollState: ScrollState = rememberScrollState()
 ) {
@@ -40,6 +42,9 @@ fun HomeScreen(
             modifier = Modifier.aspectRatio(16f / 9f),
             contentScale = ContentScale.Crop,
             contentDescription = null
+        )
+        TakeATour(
+            takeTourOnClick = takeTourClickAction
         )
         Text(
             stringResource(R.string.description_text),
@@ -60,12 +65,16 @@ fun HomeScreen(
 
 @Composable
 fun TakeATour(
+    takeTourOnClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Box(
         modifier = modifier
             .fillMaxWidth()
             .background(MaterialTheme.colorScheme.tertiary)
+            .clickable {
+                takeTourOnClick()
+            }
     ) {
         Text(
             stringResource(id = R.string.take_a_tour).uppercase(),
@@ -84,6 +93,8 @@ fun TakeATour(
 @Composable
 fun HomeScreenPreview() {
     AppTheme {
-        HomeScreen()
+        HomeScreen(
+            takeTourClickAction = {}
+        )
     }
 }
--- app/src/main/java/com/example/two/trees/ui/compose/TwoTreesNavHost.kt
+++ app/src/main/java/com/example/two/trees/ui/compose/TwoTreesNavHost.kt
@@ -17,7 +17,11 @@ fun TwoTreesNavHost(
         modifier = modifier
     ) {
         composable(route = "home") {
-            HomeScreen()
+            HomeScreen(
+                takeTourClickAction = {
+                    navController.navigate("tours")
+                }
+            )
         }
         composable(route = "tours") {
             ToursScreen()
